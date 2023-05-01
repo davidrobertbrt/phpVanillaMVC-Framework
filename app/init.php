cb0d34ccb15a59dafd288cc714871e3dcb7cba25
davidrobertbrt <84999955+davidrobertbrt@users.noreply.github.com>
2023-05-02 00:39:42 +0300

    commenting request.php and removing unnecessary lines

--- app/init.php
+++ app/init.php
@@ -2,14 +2,12 @@
 /**
  * Bootstraping for the modules responsable for the MVC Arhitecture.
  */
-
 require_once 'core/Router.php';
 require_once 'core/Request.php';
 require_once 'core/Controller.php';
 
 $router = Router::getInstance();
 $request = parseRequest();
-
 
 $router->route(
     $request['descriptor'],
