295c324cca0bb2a63ae86dae5dafcf05fa0a26c3
davidrobertbrt <84999955+davidrobertbrt@users.noreply.github.com>
2023-05-02 18:27:17 +0300

    Configuration file for database access.

---
+++ app/config/DbConfig.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * File used to configure the database connection. 
+ * For the data-access abstractization layer we will use PHP PDO with MySql engine.
+ * Change the DB_HOST,DB_NAME,DB_USER,DB_PASS to your own configuration. 
+ */
+define("DB_HOST","localhost");
+define("DB_NAME","weather");
+define("DB_USER","simplemvc");
+define("DB_PASS","asimplemvc");
