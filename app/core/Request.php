5d889c1bb1908fcd7935d2eb24d5400c75cca39c
davidrobertbrt <84999955+davidrobertbrt@users.noreply.github.com>
2023-05-09 00:45:04 +0300

    Implementing middlewares and Request/Response obj.

--- app/core/Request.php
+++ app/core/Request.php
@@ -10,25 +10,48 @@
  * the 'GET' key and POST parameters stored in the 'POST' key. The GET parameters are obtained
  * by taking all elements of the URL array after the first two. The POST parameters are obtained
  * from the $_POST superglobal variable if the HTTP method is POST.
- * The function returns an associative array with the following keys:
+ * The function returns an Request object with the following properties:
  * - descriptor: a string in the format "controller@action"
  * - method: the HTTP method
  * - data: an associative array containing the GET and POST parameters
- * @return array An associative array containing parsed information about the request.
+ * @return Request An object containing parsed information about the request.
 */
-function parseRequest(){
-    $uri = explode('/',filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
-    $controller = $uri[0] ?? null;
-    $action = $uri[1] ?? null;
-    $method = $_SERVER['REQUEST_METHOD'];
-    $requestData = array(
-        'GET' => (count($uri) > 2) ? array_values(array_slice($uri,2)) : array(),
-        'POST' => ($_SERVER['REQUEST_METHOD'] === 'POST') ? $_POST : array()
-    );
 
-    return array(
-        'descriptor' => $controller . '@' . $action,
-        'method' => $method,
-        'data' => $requestData
-    );
+class Request{
+    private $descriptor;
+    private $method;
+    private $data;
+
+    public function __construct($descriptor,$method,$data)
+    {
+        $this->descriptor = $descriptor;
+        $this->method = $method;
+        $this->data = $data;
+    }
+
+    public static function parse()
+    {
+        $uri = explode('/',filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
+        $controller = $uri[0] ?? null;
+        $action = $uri[1] ?? null;
+        $method = $_SERVER['REQUEST_METHOD'];
+        $requestData = array(
+            'GET' => (count($uri) > 2) ? array_values(array_slice($uri,2)) : array(),
+            'POST' => ($_SERVER['REQUEST_METHOD'] === 'POST') ? $_POST : array()
+        );
+    
+        return new self($controller . '@' . $action, $method, $requestData);
+    }
+
+    public function getDescriptor() {
+        return $this->descriptor;
+    }
+
+    public function getMethod() {
+        return $this->method;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
 }
