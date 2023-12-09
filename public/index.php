704b3b9e029d93379df66ec45115f82e0dc4e553
David - Robert Bratosin <84999955+davidrobertbrt@users.noreply.github.com>
2023-12-09 23:07:43 +0200

    change log

--- public/index.php
+++ public/index.php
@@ -1,6 +1,27 @@
 <?php
+
 /**
- * Security measure to protect the source code from the public view of the website.
+ * This file serves as a security measure to safeguard the source code from public access on the website.
+ * It ensures that the initialization script is required before any further processing
  */
 
 require_once '../app/init.php';
+
+/**
+ * PHPVanillaMVC-Framework
+ * 
+ * Author: David - Robert Bratosin
+ * @version 0.2.0
+ * 
+ * Changes from the last version:
+ *
+ * - Added autoloading for controllers and middlewares
+ * - Improved error handling in the Routing, Request classes
+ * - New URL structure for the descriptors
+ * - New mapping for better readibility
+ * - Now the router supports callable functions as handlers
+ * 
+ * Deprecated functionality:
+ * - The cookie class is no longer present as it will be rewritten in a new module for session managing.
+ * 
+ */
