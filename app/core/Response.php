301045575dca6d2b55151453b7cf1f3fda67f379
David - Robert Bratosin <84999955+davidrobertbrt@users.noreply.github.com>
2023-12-09 23:03:54 +0200

    New response class with added functionality of sending headers

--- app/core/Response.php
+++ app/core/Response.php
@@ -1,22 +1,77 @@
 <?php
 
-final class Response{
+/**
+ * Response Class
+ * 
+ * This class represents an HTTP response and provides methods to set the
+ * status code, content, and headers. It also includes additional methods for
+ * common response scenarios such as sending JSON or HTML content.
+ * 
+ * @final
+ */
+
+final class Response
+{
     private $statusCode;
     private $content;
+    private $headers;
 
-    public function __construct($content, $statusCode)
+    public function __construct($content, $statusCode = 200, $headers = [])
     {
         $this->statusCode = $statusCode;
         $this->content = $content;
+        $this->headers = $headers;
     }
 
-
-    public function send(){
-        //HTTP OK.
-        $this->statusCode ??= 200;
+    public function send()
+    {
+        // Set HTTP status code
         http_response_code($this->statusCode);
+        
+        // Set headers
+        if(count($this->headers) > 0){
+            $this->setHeaders();
+        }
 
-        if(isset($this->content))
+        // Output content
+        if (isset($this->content)) {
             echo $this->content;
+        }
+    }
+
+    private function setHeaders()
+    {
+        foreach ($this->headers as $name => $value) {
+            header("$name: $value");
+        }
+    }
+
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    public function addHeader($name, $value)
+    {
+        $this->headers[$name] = $value;
+    }
+
+    // Additional methods for common response scenarios
+
+    public function sendJson()
+    {
+        $this->addHeader('Content-Type', 'application/json');
+        $this->send();
+    }
+
+    public function sendHtml()
+    {
+        $this->addHeader('Content-Type', 'text/html');
+        $this->send();
     }
 }
