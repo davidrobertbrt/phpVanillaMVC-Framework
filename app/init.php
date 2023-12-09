be2c2a73cb173956bd0382ec8c817db9d1fdaa9d
David - Robert Bratosin <84999955+davidrobertbrt@users.noreply.github.com>
2023-12-09 23:16:12 +0200

    Added base model class for accessing database

--- app/init.php
+++ app/init.php
@@ -20,6 +20,7 @@ require_once 'core/Response.php';
 require_once 'core/Router.php';
 require_once 'core/Controller.php';
 require_once 'core/Middleware.php';
+require_once 'core/Model.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
